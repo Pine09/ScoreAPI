33294b01afcbd4c4a6f6649f40abc8e441016531
Arch-Student <wilzznic05@gmail.com>
2017-06-16 15:21:28 +0700

    Model added

---
+++ app/konsentrasi.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class konsentrasi extends Model
+{
+    protected $table = "konsentrasi";
+}
---
+++ app/matkul.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class matkul extends Model
+{
+    protected $table = "matkul";
+}
