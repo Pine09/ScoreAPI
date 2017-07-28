5de6804469ac85661fe4b32e92daff87fd243f64
ErikJustian <justin.rikz8@gmail.com>
2017-07-28 09:02:01 +0700

    Mengganti JadwalSeeder

--- database/seeds/JadwalSeeder.php
+++ database/seeds/JadwalSeeder.php
@@ -16,35 +16,35 @@ class JadwalSeeder extends Seeder
            'dosen_id' => '4',
            'matkul_id' => '1',
            'day' => 'Monday',
-           'status' => 'Regular',
+           'status' => 'On Going',
            'created_at' => Carbon::now()
         ]);
         DB::table('jadwal')->insert([
            'dosen_id' => '1',
            'matkul_id' => '2',
            'day' => 'Tuesday',
-           'status' => 'Regular',
+           'status' => 'On Going',
            'created_at' => Carbon::now()
         ]);
         DB::table('jadwal')->insert([
            'dosen_id' => '2',
            'matkul_id' => '5',
            'day' => 'Wednesday',
-           'status' => 'Regular',
+           'status' => 'On Going',
            'created_at' => Carbon::now()
         ]);
         DB::table('jadwal')->insert([
            'dosen_id' => '3',
            'matkul_id' => '3',
            'day' => 'Thursday',
-           'status' => 'Regular',
+           'status' => 'On Going',
            'created_at' => Carbon::now()
         ]);
         DB::table('jadwal')->insert([
            'dosen_id' => '5',
            'matkul_id' => '4',
            'day' => 'Friday',
-           'status' => 'Regular',
+           'status' => 'Ended',
            'created_at' => Carbon::now()
         ]);
     }
